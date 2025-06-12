f81ca23deba1956b11ad353ce906d653e1403694
Julio0Cesar <juliocesar70777077@gmail.com>
2025-06-12 11:16:44 -0300

    feat: adicionando tratamento global de erros

--- backend/src/modules/cliente/cliente.controller.ts
+++ backend/src/modules/cliente/cliente.controller.ts
@@ -1,12 +1,18 @@
 import { Request, Response } from "express"
-import { criarClientenoBancoAsync } from "./cliente.service"
+import { criarClienteNoBancoAsync, listarClientesDoBancoAsync } from "./cliente.service"
+import { erroHandling } from "../../shared/errors/erro-handling"
 
 export async function criarClienteAsync(req: Request, res: Response) {
   try {
-    const novoCliente = await criarClientenoBancoAsync(req.body)
-    return res.status(201).json(novoCliente)
+    const novoCliente = await criarClienteNoBancoAsync(req.body)
+    return res.status(201).json({ message: "Usuário criado com sucesso",novoCliente})
+
   } catch (error: any) {
-    console.error(error)
+    if (error instanceof erroHandling) {
+      return res.status(error.statusCode).json({ message: error.message })
+    }
+
+    console.error("Erro não tratado:", error)
     return res.status(500).json({ message: "Erro interno no servidor" })
   }
 }
@@ -14,7 +20,7 @@ export async function criarClienteAsync(req: Request, res: Response) {
 
 export async function listClients(req: Request, res: Response) {
   try {
-    const clientes = await ClientsService.listClients()
+    const clientes = await listarClientesDoBancoAsync()
     return res.status(200).json(clientes)
   } catch (error) {
     console.error(error)
--- backend/src/modules/cliente/cliente.service.ts
+++ backend/src/modules/cliente/cliente.service.ts
@@ -1,37 +1,30 @@
 import { AppDataSource } from "../../ormconfig"
 import { Cliente } from "../../core/entities/Cliente"
+import { erroHandling } from "../../shared/errors/erro-handling"
 
 interface ClienteInput {
   nome?: string
   email?: string
   cnpj?: string
+  senha?: string
 }
 
-export async function criarClientenoBancoAsync(data: ClienteInput) {
-  const { nome, email, cnpj } = data
-
-  if (!nome || !email || !cnpj) {
-    throw new Error("Campos obrigatórios faltando")
-  }
+export async function criarClienteNoBancoAsync(data: ClienteInput) {
+  const { nome, email, cnpj, senha } = data
 
   const clienteRepo = AppDataSource.getRepository(Cliente)
 
-  const existingClient = await clienteRepo.findOneBy([
-    { email },
-    { cnpj }
-  ])
-
+  const existingClient = await clienteRepo.findOneBy([ { email }, { cnpj } ])
   if (existingClient) {
-    throw new Error("Cliente já cadastrado")
+    throw new erroHandling("Cliente já registrado", 400)
   }
 
-  const novoCliente = clienteRepo.create({ nome, email, cnpj })
+  const novoCliente = clienteRepo.create({ nome, email, cnpj, senha })
   await clienteRepo.save(novoCliente)
-
   return novoCliente
 }
 
-export async function listClients() {
+export async function listarClientesDoBancoAsync() {
   const clienteRepo = AppDataSource.getRepository(Cliente)
   const clientes = await clienteRepo.find()
   return clientes
