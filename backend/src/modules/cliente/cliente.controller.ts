bd829815c818096b4ffe7d5d3116b51ebc84359a
Julio0Cesar <juliocesar70777077@gmail.com>
2025-06-14 18:57:52 -0300

    fix: removendo senhahash do cliente no output

--- backend/src/modules/cliente/cliente.controller.ts
+++ backend/src/modules/cliente/cliente.controller.ts
@@ -2,12 +2,13 @@ import { Request, Response } from "express"
 import { registrarNovoCliente, listarClientes } from "./cliente.service"
 import { ClienteDTO } from "./dto/create-cliente.dto"
 import { logger } from "../../shared/logs/logger"
+import { mapClienteToOutput } from "./dto/output-cliente.dto"
 
 // POST: /clients
 export async function handleCriarCliente(req: Request<{}, {}, ClienteDTO>, res: Response) {
   const novoCliente = await registrarNovoCliente(req.body)
   logger.info(`Cliente: ${novoCliente.nome}, criado com sucesso (200 OK)`)
-  res.status(201).json({ message: "Cliente criado com sucesso", novoCliente })
+  res.status(201).json({ message: "Cliente criado com sucesso", novoCliente: mapClienteToOutput(novoCliente)})
 }
 
 // GET: /clients
